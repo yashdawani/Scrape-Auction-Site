c29322e3490b479733703c41c8730eca9a45018c
Yash Dawani <yashdawani386@gmail.com>
2022-09-27 18:11:48 +1000

    Created Scraper class

--- scraper-optimised.py
+++ scraper-optimised.py
@@ -4,27 +4,32 @@ from selenium import webdriver
 from pathlib import Path
 from bs4 import BeautifulSoup as soup
 
-path = Path(__file__).parent / "chromedriver"
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-# attach chrome_options to driver
-driver = webdriver.Chrome(path, chrome_options=chrome_options)
 
-driver.get(
-    "https://liquidassets.hibid.com/catalog/397846/46-x-macbook-pro--new-65--televisions--monitors-and-furniture/?ipp=100")
-pageSource = driver.page_source
-soup_data = soup(pageSource, 'lxml')
-baseUrl = "https://liquidassets.hibid.com"
-# soup find tag h2
-requested_data = soup_data.find_all(
-    'div', {'class': 'lot-tile col-md-3 col-lg-2 p-x-xs px-md-1'})
-data = {}
-for row in requested_data:
-    text = row.contents[1].text
-    Title = row.contents[1].find('a').text
-    url = baseUrl + row.contents[1].find('a').attrs.get('href')
-    lines = re.sub(
-        '\n*\n', '\n', requested_data[0].contents[1].text.replace('\t', '')).split('\n')
-    highest_bid = lines[13]
-    data[url] = [Title, highest_bid]
+class scraper():
+    def __init__(self) -> None:
+        self.path = Path(__file__).parent / "chromedriver"
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        # attach chrome_options to driver
+        self.driver = webdriver.Chrome(
+            self.path, chrome_options=chrome_options)
+
+    def startScraper(self):
+        self.driver.get(
+            "https://liquidassets.hibid.com/catalog/397846/46-x-macbook-pro--new-65--televisions--monitors-and-furniture/?ipp=100")
+        pageSource = self.driver.page_source
+        soup_data = soup(pageSource, 'lxml')
+        baseUrl = "https://liquidassets.hibid.com"
+        # soup find tag h2
+        requested_data = soup_data.find_all(
+            'div', {'class': 'lot-tile col-md-3 col-lg-2 p-x-xs px-md-1'})
+        self.data = {}
+        for row in requested_data:
+            text = row.contents[1].text
+            Title = row.contents[1].find('a').text
+            url = baseUrl + row.contents[1].find('a').attrs.get('href')
+            lines = re.sub(
+                '\n*\n', '\n', requested_data[0].contents[1].text.replace('\t', '')).split('\n')
+            highest_bid = lines[13]
+            self.data[url] = [Title, highest_bid]
