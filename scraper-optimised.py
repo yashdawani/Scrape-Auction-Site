200ce85b66525c6aed4f30d81a17faeb8f440594
pranaykhurana <pranay.khurana99@gmail.com>
2022-09-28 21:21:49 +1000

    Added emailing using gmail

--- scraper-optimised.py
+++ scraper-optimised.py
@@ -1,12 +1,14 @@
-from pathlib import Path
-import re
 from selenium import webdriver
 from pathlib import Path
 from bs4 import BeautifulSoup as soup
 from supabase import create_client, Client
 from decouple import config
-from twilio.rest import Client as tC
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import re
+import sys
+import logging
 
 class Scraper():
     def __init__(self) -> None:
@@ -22,11 +24,7 @@ class Scraper():
         self.key = config('API_KEY')
         self.supa_client: Client = create_client(self.url, self.key)
 
-        self.account_sid = config('TWILIO_ACCOUNT_SID')
-        self.auth_token = config('TWILIO_AUTH_TOKEN')
-        self.phone_number = config('TWILIO_NUMBER')
-        self.target_number = config('TARGET_NUMBER')
-        self.twilio_client = tC(self.account_sid, self.auth_token)
+        # self.sendgrid_api_key = config('SENDGRID_API_KEY')
 
     def startScraper(self):
         self.driver.get(
@@ -52,7 +50,7 @@ class Scraper():
             item = {}
             item['title'] = title
             item['url'] = url
-            # item['price'] = int(float(highest_bid.replace(" ", "").split("AUD")[0].replace(",", "")))
+
             item['price'] = re.findall(r'\d+', highest_bid.replace(',', ""))[0]
             item['bids'] = int(bids)
             self.data.append(item)
@@ -61,20 +59,55 @@ class Scraper():
         for item in self.data:
             query = self.supa_client.table("Products").insert(item).execute()
 
-    def sendMessage(self):
+    def getData(self):
         try:
             for item in self.data:
-                query = self.supa_client.table("Products")
-                if query:
-                    pass
+                message_queue = []
+                query : Client = self.supa_client.table("Products").select("*").filter("url", "ilike", f"{item['url']}").order("created_at", desc=True).execute()
+                previous = query.data[0]
+                if item['price'] > previous['price']:
+                    message = f'Price of {item["title"]} increased from {previous["price"]} to {item["price"]} \n Click here to view {item["url"]} \n'
+                    message_queue.append(message)
+                
         except Exception as e:
             with open('logfile.txt', 'a+') as file:
                 file.write("hello")
                 file.write('\n')
-                file.close()
 
+        return message_queue
 
+    def sendMessage(self):
+        emailTemplate = ''
+        messages = self.getData()
+        if len(messages) == 0:
+            from datetime import datetime
+            now = datetime.now()
+            sys.exit(f"No updates found at {now}")
+    
+        for message in messages:
+            emailTemplate += message
 
+        try:
+            fromEmail = config('FROM_EMAIL')
+            fromPassword = config('FROM_PASSWORD')
+            toEmail = config('TO_EMAIL')
+
+            message = MIMEMultipart()
+            message['From'] = fromEmail
+            message['To'] = toEmail
+            message['Subject'] = 'Auction Price Updates' 
+
+            message.attach(MIMEText(emailTemplate, 'plain'))
+
+            session = smtplib.SMTP('smtp.gmail.com', 587)
+            session.starttls()
+            session.login(fromEmail, fromPassword)
+            text = message.as_string()
+            session.sendmail(fromEmail, toEmail, text)
+            session.quit()
+
+        except Exception as e:
+            print('Something went wrong...', e)
 
     def _run(self):
         self.startScraper()
